72a5931a34db104b4bfd54fa5149b14d7a9f8127
haneeshraj <haneeshraj.banisetti@gmail.com>
2024-03-12 23:48:23 -0400

    Removed db data

--- src/components/Sections/Home/Footer/Footer.tsx
+++ src/components/Sections/Home/Footer/Footer.tsx
@@ -9,11 +9,7 @@ import Link from "next/link";
 import Reveal from "@/components/Reveal/Reveal";
 import { getInfo } from "@/functions/data";
 
-export const dynamic = "force-dynamic";
-
 const Footer = async () => {
-  const socials = await getInfo();
-
   return (
     <Reveal>
       <div className={styles.footer}>
@@ -35,76 +31,28 @@ const Footer = async () => {
               <Reveal>
                 <div className={styles["contact-group"]}>
                   <p className={styles["title"]}>Email</p>
-                  <p className={styles["label"]}>
-                    {socials
-                      ? !socials.email
-                        ? INFO.email
-                        : socials.email
-                      : INFO.email}
-                  </p>
+                  <p className={styles["label"]}>{INFO.email}</p>
                 </div>
               </Reveal>
               <Reveal>
                 <div className={styles["contact-group"]}>
                   <p className={styles["title"]}>Phone Number</p>
-                  <p className={styles["label"]}>
-                    {socials
-                      ? !socials.phone
-                        ? INFO.phoneNumber
-                        : socials.phone
-                      : INFO.phoneNumber}
-                  </p>
+                  <p className={styles["label"]}>{INFO.phoneNumber}</p>
                 </div>
               </Reveal>
             </div>
             <Reveal>
               <div className={styles["icons"]}>
-                <Link
-                  href={
-                    socials
-                      ? !socials.instagram
-                        ? INFO.socials.instagram
-                        : socials.instagram
-                      : INFO.socials.instagram
-                  }
-                  target="_blank"
-                >
+                <Link href={INFO.socials.instagram} target="_blank">
                   <Logo.instagram className={styles["icon"]} />
                 </Link>
-                <Link
-                  href={
-                    socials
-                      ? !socials.twitter
-                        ? INFO.socials.twitter
-                        : socials.twitter
-                      : INFO.socials.twitter
-                  }
-                  target="_blank"
-                >
+                <Link href={INFO.socials.twitter} target="_blank">
                   <Logo.twitter className={styles["icon"]} />
                 </Link>
-                <Link
-                  href={
-                    socials
-                      ? !socials.youtube
-                        ? INFO.socials.youtube
-                        : socials.youtube
-                      : INFO.socials.youtube
-                  }
-                  target="_blank"
-                >
+                <Link href={INFO.socials.youtube} target="_blank">
                   <Logo.youtube className={styles["icon"]} />
                 </Link>
-                <Link
-                  href={
-                    socials
-                      ? !socials.twitter
-                        ? INFO.socials.twitter
-                        : socials.twitter
-                      : INFO.socials.spotify
-                  }
-                  target="_blank"
-                >
+                <Link href={INFO.socials.spotify} target="_blank">
                   <Logo.spotify className={styles["icon"]} />
                 </Link>
               </div>
