bfebdcd60305e82cdc8d1ad72ff64cc774b13c3e
haneeshraj <haneeshraj.banisetti@gmail.com>
2024-03-13 13:10:48 -0400

    feat: Add revalidate path and get info

--- src/components/Sections/Home/Footer/Footer.tsx
+++ src/components/Sections/Home/Footer/Footer.tsx
@@ -32,28 +32,76 @@ export const Footer = async () => {
               <Reveal>
                 <div className={styles["contact-group"]}>
                   <p className={styles["title"]}>Email</p>
-                  <p className={styles["label"]}>{socials?.email}</p>
+                  <p className={styles["label"]}>
+                    {socials?.email
+                      ? !socials.email
+                        ? INFO.email
+                        : socials.email
+                      : INFO.email}
+                  </p>
                 </div>
               </Reveal>
               <Reveal>
                 <div className={styles["contact-group"]}>
                   <p className={styles["title"]}>Phone Number</p>
-                  <p className={styles["label"]}>{INFO.phoneNumber}</p>
+                  <p className={styles["label"]}>
+                    {socials?.phone
+                      ? !socials.phone
+                        ? INFO.phoneNumber
+                        : socials.phone
+                      : INFO.phoneNumber}
+                  </p>
                 </div>
               </Reveal>
             </div>
             <Reveal>
               <div className={styles["icons"]}>
-                <Link href={INFO.socials.instagram} target="_blank">
+                <Link
+                  href={
+                    socials?.instagram
+                      ? !socials.instagram
+                        ? INFO.socials.instagram
+                        : socials.instagram
+                      : INFO.socials.instagram
+                  }
+                  target="_blank"
+                >
                   <Logo.instagram className={styles["icon"]} />
                 </Link>
-                <Link href={INFO.socials.twitter} target="_blank">
+                <Link
+                  href={
+                    socials?.twitter
+                      ? !socials.twitter
+                        ? INFO.socials.twitter
+                        : socials.twitter
+                      : INFO.socials.twitter
+                  }
+                  target="_blank"
+                >
                   <Logo.twitter className={styles["icon"]} />
                 </Link>
-                <Link href={INFO.socials.youtube} target="_blank">
+                <Link
+                  href={
+                    socials?.youtube
+                      ? !socials.youtube
+                        ? INFO.socials.youtube
+                        : socials.youtube
+                      : INFO.socials.youtube
+                  }
+                  target="_blank"
+                >
                   <Logo.youtube className={styles["icon"]} />
                 </Link>
-                <Link href={INFO.socials.spotify} target="_blank">
+                <Link
+                  href={
+                    socials?.spotify
+                      ? !socials.spotify
+                        ? INFO.socials.spotify
+                        : socials.spotify
+                      : INFO.socials.spotify
+                  }
+                  target="_blank"
+                >
                   <Logo.spotify className={styles["icon"]} />
                 </Link>
               </div>
